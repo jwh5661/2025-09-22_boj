a0f6d1aa76ecd515b88827b4064f6626f9eb6579
jwh5661 <jwh5661@gmail.com>
2026-02-26 02:49:05 +0900

    2026/02/25 upload
    new : 9019
    D+3 review : 13460, 1213
    D+14 review : 6118, 2675

---
+++ ide/vs2022/CT_Practice_20250922/boj_09019_DSLR.cpp
@@ -0,0 +1,116 @@
+﻿/*
+BOJ 9019 — DSLR (DSLR)
+Topic: BFS / Shortest Path / Path Reconstruction
+Folder: graph/bfs/
+
+⏱ 시간복잡도: O(10000) (각 테스트 케이스마다 상태 최대 10000개)
+💾 공간복잡도: O(10000)
+
+📌 핵심 아이디어
+- 0~9999를 정점으로 보고 DSLR 연산을 간선으로 두는 무가중치 최단거리 문제.
+- BFS로 최단 경로를 찾고, visited에 (사용한 연산, 이전 숫자)를 저장해 경로를 역추적한다.
+
+⚠️ 주의할 점
+- S 연산: 0이면 9999, 그 외는 n-1 (1은 0이 되어야 함).
+- 범위는 항상 0~9999 유지.
+- 범위 체크/모듈 연산으로 배열 범위 밖 접근(out of bounds, 배열 범위 밖 접근) 방지.
+
+✔️ 지문 힌트
+- “최소 연산 횟수” → BFS
+- “명령어 출력” → parent/operation 기록 후 역추적
+*/
+
+// 2026-02-25 ORIGINAL
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int a, b;
+		cin >> a >> b;
+
+		vector<pair<char, int>> visited(10000, { '.', -1 });
+		queue<int> q;
+		q.emplace(a);
+		visited[a] = { 'X', a };
+
+		while (!q.empty())
+		{
+			int cur = q.front(); q.pop();
+			//cout << cur << '\n';
+			if (cur == b) break;
+
+			for (int d = 0; d < 4; d++)
+			{
+				int next;
+				switch (d)
+				{
+				case 0:
+					next = (cur * 2) % 10000;
+					break;
+				case 1:
+					if (cur == 0)
+						next = 9999;
+					else
+						next = cur - 1;
+					break;
+				case 2:
+					next = (cur % 1000) * 10 + (cur / 1000);
+					break;
+				case 3:
+					next = (cur % 10) * 1000 + (cur / 10);
+					break;
+				}
+				
+				if (visited[next].second != -1) continue;
+
+				switch (d)
+				{
+				case 0:
+					visited[next].first = 'D';
+					break;
+				case 1:
+					visited[next].first = 'S';
+					break;
+				case 2:
+					visited[next].first = 'L';
+					break;
+				case 3:
+					visited[next].first = 'R';
+					break;
+				}
+				q.push(next);
+				visited[next].second = cur;
+			}
+		}
+
+		stack<char> stk;
+		int i = b;
+		while (i != a)
+		{
+			stk.push(visited[i].first);
+			i = visited[i].second;
+		}
+		
+		while (!stk.empty())
+		{
+			cout << stk.top();
+			stk.pop();
+		}
+		cout << '\n';
+	}
+}
--- ide/vs2022/CT_Practice_20250922/boj_13460_Bead_Escape_2.cpp
+++ ide/vs2022/CT_Practice_20250922/boj_13460_Bead_Escape_2.cpp
@@ -21,35 +21,41 @@ Folder: graph/bfs
 - “최대 10번 기울이기” + “최소 횟수” → BFS가 정석.
 */
 
-// 2026-02-20 ORIGINAL
+// 2026-02-25 D+3 REIVEW
 
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <string>
 
 using namespace std;
 
-struct State {
+int Move(int& x, int& y, int dx, int dy);
+
+vector<vector<char>> board;
+int dx[4] = { 0, 0, -1, 1 };
+int dy[4] = { -1, 1, 0, 0 };
+int n, m;
+
+struct State
+{
 	int rx, ry, bx, by, cnt;
 
 	State(int nrx, int nry, int nbx, int nby, int ncnt)
 		:rx(nrx), ry(nry), bx(nbx), by(nby), cnt(ncnt) {
-	}
+	};
 };
-
-bool visited[11][11][11][11];
-int dx[4] = { 0, 0, -1, 1 };
-int dy[4] = { -1, 1, 0, 0 };
 
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int n, m, start_rx = 0, start_ry = 0, start_bx = 0, start_by = 0;
+	int start_rx = 0, start_ry = 0, start_bx = 0, start_by = 0;
 	cin >> n >> m;
 
-	vector<vector<char>> board(n, vector<char>(m));
+	board = vector<vector<char>>(n, vector<char>(m));
+	vector<bool> visited(n * m * n * m, false);
 	queue<State> q;
 
 	for (int i = 0; i < n; i++)
@@ -59,87 +65,210 @@ int main()
 		for (int j = 0; j < m; j++)
 		{
 			board[i][j] = str[j];
-			if (board[i][j] == 'B')
+
+			if (board[i][j] == 'R')
+			{
+				start_rx = i; 
+				start_ry = j;
+				board[i][j] = '.';
+			}
+			else if (board[i][j] == 'B')
 			{
 				start_bx = i;
 				start_by = j;
-			}
-			else if (board[i][j] == 'R')
-			{
-				start_rx = i;
-				start_ry = j;
+				board[i][j] = '.';
 			}
 		}
+			
 	}
 
-	q.emplace(State(start_rx, start_ry, start_bx, start_by, 0));
-	visited[start_rx][start_ry][start_bx][start_by] = true;
+	q.emplace(start_rx, start_ry, start_bx, start_by, 0);
+	visited[start_rx * m * n * m + 
+		start_ry * n * m + start_bx * m + start_by] = true;
 
 	while (!q.empty())
 	{
 		State cur = q.front(); q.pop();
-		int crx = cur.rx, cry = cur.ry, cbx = cur.bx, cby = cur.by, ccnt = cur.cnt;
 
-		if (ccnt >= 10) continue;
-		for (int i = 0; i < 4; i++)
+		if (cur.cnt >= 10) continue;
+
+		for (int d = 0; d < 4; d++)
 		{
-			int nrx = crx + dx[i];
-			int nry = cry + dy[i];
-			int nbx = cbx + dx[i];
-			int nby = cby + dy[i];
+			int nrx = cur.rx;
+			int nry = cur.ry;
+			int nbx = cur.bx;
+			int nby = cur.by;
 			int r_dist = 0, b_dist = 0;
-			bool r_escape = false, b_escape = false;
 
-			while (board[nrx][nry] != '#' && board[nrx][nry] != 'O')
+			r_dist = Move(nrx, nry, dx[d], dy[d]);
+
+			b_dist = Move(nbx, nby, dx[d], dy[d]);
+
+			if (board[nbx + dx[d]][nby + dy[d]] == 'O') continue;
+
+			if (board[nrx + dx[d]][nry + dy[d]] == 'O')
 			{
-				r_dist++;
-				nrx += dx[i];
-				nry += dy[i];
-			}
-
-			while (board[nbx][nby] != '#' && board[nbx][nby] != 'O')
-			{
-				b_dist++;
-				nbx += dx[i];
-				nby += dy[i];
-			}
-
-			if (board[nrx][nry] == 'O')
-				r_escape = true;
-
-			if (board[nbx][nby] == 'O')
-				b_escape = true;
-
-			if (b_escape) continue;
-			if (r_escape)
-			{
-				cout << ccnt + 1 << '\n';
+				cout << cur.cnt + 1 << '\n';
 				return 0;
 			}
+
 			if (nrx == nbx && nry == nby)
 			{
-				if (r_dist < b_dist)
+				if (r_dist > b_dist)
 				{
-					nbx -= dx[i];
-					nby -= dy[i];
+					nrx -= dx[d];
+					nry -= dy[d];
 				}
 				else
 				{
-					nrx -= dx[i];
-					nry -= dy[i];
+					nbx -= dx[d];
+					nby -= dy[d];
 				}
 			}
 
-			nrx -= dx[i];
-			nry -= dy[i];
-			nbx -= dx[i];
-			nby -= dy[i];
-
-			if (visited[nrx][nry][nbx][nby]) continue;
-			q.emplace(nrx, nry, nbx, nby, ccnt + 1);
-			visited[nrx][nry][nbx][nby] = true;
+			int idx = nrx * m * n * m + nry * n * m + nbx * m + nby;
+			if (visited[idx]) continue;
+			q.emplace(nrx, nry, nbx, nby, cur.cnt + 1);
+			visited[idx] = true;
 		}
 	}
 
 	cout << -1 << '\n';
 }
+
+int Move(int& x, int& y, int dx, int dy)
+{
+	int dist = 0;
+
+	while (board[x + dx][y + dy] == '.')
+	{
+		x += dx;
+		y += dy;
+		dist++;
+	}
+
+	return dist;
+}
+
+// 2026-02-20 ORIGINAL
+
+// #include <iostream>
+// #include <vector>
+// #include <queue>
+// 
+// using namespace std;
+// 
+// struct State {
+// 	int rx, ry, bx, by, cnt;
+// 
+// 	State(int nrx, int nry, int nbx, int nby, int ncnt)
+// 		:rx(nrx), ry(nry), bx(nbx), by(nby), cnt(ncnt) {
+// 	}
+// };
+// 
+// bool visited[11][11][11][11];
+// int dx[4] = { 0, 0, -1, 1 };
+// int dy[4] = { -1, 1, 0, 0 };
+// 
+// int main()
+// {
+// 	ios::sync_with_stdio(false);
+// 	cin.tie(nullptr);
+// 
+// 	int n, m, start_rx = 0, start_ry = 0, start_bx = 0, start_by = 0;
+// 	cin >> n >> m;
+// 
+// 	vector<vector<char>> board(n, vector<char>(m));
+// 	queue<State> q;
+// 
+// 	for (int i = 0; i < n; i++)
+// 	{
+// 		string str;
+// 		cin >> str;
+// 		for (int j = 0; j < m; j++)
+// 		{
+// 			board[i][j] = str[j];
+// 			if (board[i][j] == 'B')
+// 			{
+// 				start_bx = i;
+// 				start_by = j;
+// 			}
+// 			else if (board[i][j] == 'R')
+// 			{
+// 				start_rx = i;
+// 				start_ry = j;
+// 			}
+// 		}
+// 	}
+// 
+// 	q.emplace(State(start_rx, start_ry, start_bx, start_by, 0));
+// 	visited[start_rx][start_ry][start_bx][start_by] = true;
+// 
+// 	while (!q.empty())
+// 	{
+// 		State cur = q.front(); q.pop();
+// 		int crx = cur.rx, cry = cur.ry, cbx = cur.bx, cby = cur.by, ccnt = cur.cnt;
+// 
+// 		if (ccnt >= 10) continue;
+// 		for (int i = 0; i < 4; i++)
+// 		{
+// 			int nrx = crx + dx[i];
+// 			int nry = cry + dy[i];
+// 			int nbx = cbx + dx[i];
+// 			int nby = cby + dy[i];
+// 			int r_dist = 0, b_dist = 0;
+// 			bool r_escape = false, b_escape = false;
+// 
+// 			while (board[nrx][nry] != '#' && board[nrx][nry] != 'O')
+// 			{
+// 				r_dist++;
+// 				nrx += dx[i];
+// 				nry += dy[i];
+// 			}
+// 
+// 			while (board[nbx][nby] != '#' && board[nbx][nby] != 'O')
+// 			{
+// 				b_dist++;
+// 				nbx += dx[i];
+// 				nby += dy[i];
+// 			}
+// 
+// 			if (board[nrx][nry] == 'O')
+// 				r_escape = true;
+// 
+// 			if (board[nbx][nby] == 'O')
+// 				b_escape = true;
+// 
+// 			if (b_escape) continue;
+// 			if (r_escape)
+// 			{
+// 				cout << ccnt + 1 << '\n';
+// 				return 0;
+// 			}
+// 			if (nrx == nbx && nry == nby)
+// 			{
+// 				if (r_dist < b_dist)
+// 				{
+// 					nbx -= dx[i];
+// 					nby -= dy[i];
+// 				}
+// 				else
+// 				{
+// 					nrx -= dx[i];
+// 					nry -= dy[i];
+// 				}
+// 			}
+// 
+// 			nrx -= dx[i];
+// 			nry -= dy[i];
+// 			nbx -= dx[i];
+// 			nby -= dy[i];
+// 
+// 			if (visited[nrx][nry][nbx][nby]) continue;
+// 			q.emplace(nrx, nry, nbx, nby, ccnt + 1);
+// 			visited[nrx][nry][nbx][nby] = true;
+// 		}
+// 	}
+// 
+// 	cout << -1 << '\n';
+// }
