b4c6512ef21381d3c31aead1e2c71dd144e2f10e
jwh5661 <jwh5661@gmail.com>
2026-04-10 23:27:15 +0900

    2026/04/10 upload
    reboot : 1753, 1504

--- ide/vs2022/CT_Practice_20250922/boj_01504_Specific_Shortest_Path.cpp
+++ ide/vs2022/CT_Practice_20250922/boj_01504_Specific_Shortest_Path.cpp
@@ -1,4 +1,4 @@
-// 2026-03-11 (수)
+// 2026-04-10 (금)
 
 #include <iostream>
 #include <vector>
@@ -7,51 +7,63 @@
 
 using namespace std;
 
-vector<long long> Dijkstra
-(int start, int n, const vector<vector<pair<int, int>>>& con_list);
+struct Edge {
+	int dist, arr;
+
+	Edge(int ndist, int narr)
+		:dist(ndist), arr(narr) {
+	};
+
+	bool operator>(const Edge& other) const {
+		return dist > other.dist;
+	}
+};
+
+void Dijkstra(int start);
 
 const int mx_dist = 1e9;
+const int mx_v = 801;
+const long long INF = 1e9;
+int n, e;
+vector<Edge> adj[mx_v];
+long long d[mx_v], p1[mx_v], p2[mx_v], p3[mx_v];
 
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int n, e;
 	cin >> n >> e;
-
-	vector<vector<pair<int, int>>> con_list(n + 1);
 
 	for (int i = 0; i < e; i++)
 	{
 		int dep, arr, dist;
 		cin >> dep >> arr >> dist;
 
-		con_list[dep].emplace_back(dist, arr);
-		con_list[arr].emplace_back(dist, dep);
+		adj[dep].emplace_back(dist, arr);
+		adj[arr].emplace_back(dist, dep);
 	}
 
 	int v1, v2;
 	cin >> v1 >> v2;
 
-	vector<long long> p1, p2, p3;
-
-	p1 = Dijkstra(1, n, con_list);
-	p2 = Dijkstra(v1, n, con_list);
-	p3 = Dijkstra(n, n, con_list);
+	Dijkstra(1);
+	copy(d, d + n + 1, p1);	// 배열 복사 함수
+	Dijkstra(v1);
+	copy(d, d + n + 1, p2);
+	Dijkstra(n);
+	copy(d, d + n + 1, p3);
 
 	long long ans = min(p1[v1] + p2[v2] + p3[v2], p1[v2] + p2[v2] + p3[v1]);
-	
+
 	if (ans >= mx_dist) cout << -1 << '\n';
 	else cout << ans << '\n';
 }
 
-vector<long long> Dijkstra(int start, int n, const vector<vector<pair<int, int>>>& con_list)
+void Dijkstra(int start)
 {
-	vector<long long> d(n + 1, mx_dist);
-	priority_queue
-		<pair<long long, int>, vector<pair<long long, int>>,
-		greater<pair<long long, int>>> pq;
+	fill(d, d + n + 1, INF);
+	priority_queue<Edge, vector<Edge>, greater<Edge>> pq;
 
 	d[start] = 0;
 	pq.emplace(0, start);
@@ -59,15 +71,15 @@ vector<long long> Dijkstra(int start, int n, const vector<vector<pair<int, int>>
 	while (!pq.empty())
 	{
 		auto cur = pq.top(); pq.pop();
-		long long cd = cur.first;
-		int cnode = cur.second;
+		long long cd = cur.dist;
+		int cnode = cur.arr;
 
 		if (cd > d[cnode]) continue;
 
-		for (auto next : con_list[cnode])
+		for (const auto& next : adj[cnode])
 		{
-			long long nd = cd + next.first;
-			int nnode = next.second;
+			long long nd = cd + next.dist;
+			int nnode = next.arr;
 
 			if (nd >= d[nnode]) continue;
 
@@ -75,6 +87,85 @@ vector<long long> Dijkstra(int start, int n, const vector<vector<pair<int, int>>
 			d[nnode] = nd;
 		}
 	}
-
-	return d;
 }
+
+// 2026-03-11 (수)
+
+// #include <iostream>
+// #include <vector>
+// #include <queue>
+// #include <algorithm>
+// 
+// using namespace std;
+// 
+// vector<long long> Dijkstra
+// (int start, int n, const vector<vector<pair<int, int>>>& con_list);
+// 
+// const int mx_dist = 1e9;
+// 
+// int main()
+// {
+// 	ios::sync_with_stdio(false);
+// 	cin.tie(nullptr);
+// 
+// 	int n, e;
+// 	cin >> n >> e;
+// 
+// 	vector<vector<pair<int, int>>> con_list(n + 1);
+// 
+// 	for (int i = 0; i < e; i++)
+// 	{
+// 		int dep, arr, dist;
+// 		cin >> dep >> arr >> dist;
+// 
+// 		con_list[dep].emplace_back(dist, arr);
+// 		con_list[arr].emplace_back(dist, dep);
+// 	}
+// 
+// 	int v1, v2;
+// 	cin >> v1 >> v2;
+// 
+// 	vector<long long> p1, p2, p3;
+// 
+// 	p1 = Dijkstra(1, n, con_list);
+// 	p2 = Dijkstra(v1, n, con_list);
+// 	p3 = Dijkstra(n, n, con_list);
+// 
+// 	long long ans = min(p1[v1] + p2[v2] + p3[v2], p1[v2] + p2[v2] + p3[v1]);
+// 	
+// 	if (ans >= mx_dist) cout << -1 << '\n';
+// 	else cout << ans << '\n';
+// }
+// 
+// vector<long long> Dijkstra(int start, int n, const vector<vector<pair<int, int>>>& con_list)
+// {
+// 	vector<long long> d(n + 1, mx_dist);
+// 	priority_queue
+// 		<pair<long long, int>, vector<pair<long long, int>>,
+// 		greater<pair<long long, int>>> pq;
+// 
+// 	d[start] = 0;
+// 	pq.emplace(0, start);
+// 
+// 	while (!pq.empty())
+// 	{
+// 		auto cur = pq.top(); pq.pop();
+// 		long long cd = cur.first;
+// 		int cnode = cur.second;
+// 
+// 		if (cd > d[cnode]) continue;
+// 
+// 		for (auto next : con_list[cnode])
+// 		{
+// 			long long nd = cd + next.first;
+// 			int nnode = next.second;
+// 
+// 			if (nd >= d[nnode]) continue;
+// 
+// 			pq.emplace(nd, nnode);
+// 			d[nnode] = nd;
+// 		}
+// 	}
+// 
+// 	return d;
+// }
--- ide/vs2022/CT_Practice_20250922/boj_01753_Shortest_Path.cpp
+++ ide/vs2022/CT_Practice_20250922/boj_01753_Shortest_Path.cpp
@@ -1,10 +1,26 @@
-// 2026-03-05 (¸ñ)
+// 2026-04-10 (±Ý)
 
 #include <iostream>
 #include <vector>
 #include <queue>
 
 using namespace std;
+
+struct Edge {
+	int dist, arr;
+
+	Edge(int ndist, int narr)
+		:dist(ndist), arr(narr) {
+	};
+
+	bool operator>(const Edge& other) const {
+		return dist > other.dist;
+	}
+};
+
+const int max_v = 20001;
+const int INF = 1e9;
+vector<Edge> adj_list[max_v];
 
 int main()
 {
@@ -14,47 +30,102 @@ int main()
 	int v, e, k;
 	cin >> v >> e >> k;
 
-	vector<vector<pair<int, int>>> con_list(v + 1);
-	vector<int> d(v + 1, 1e9);
-	priority_queue<pair<int, int>, 
-		vector<pair<int, int>>, greater<pair<int, int>>> pq;
+	vector<int> total_dist(v + 1, INF);
+	priority_queue<Edge, vector<Edge>, greater<Edge>> pq;
 
 	for (int i = 0; i < e; i++)
 	{
 		int dep, arr, dist;
 		cin >> dep >> arr >> dist;
 
-		con_list[dep].emplace_back(dist, arr);
+		adj_list[dep].emplace_back(dist, arr);
 	}
-
-	pq.emplace(0, k);
-	d[k] = 0;
+	
+	total_dist[k] = 0;
+	pq.emplace(total_dist[k], k);
 
 	while (!pq.empty())
 	{
 		auto cur = pq.top(); pq.pop();
-		int cd = cur.first;
-		int carr = cur.second;
+		
+		if (cur.dist > total_dist[cur.arr]) continue;
 
-		if (d[carr] < cd) continue;
-
-		for (auto next : con_list[carr])
+		for (const auto& next : adj_list[cur.arr])
 		{
-			int nd = next.first;
-			int narr = next.second;
+			if (next.dist + cur.dist >= total_dist[next.arr]) continue;
 
-			if (cd + nd >= d[narr]) continue;
-
-			pq.emplace(cd + nd, narr);
-			d[narr] = cd + nd;
+			total_dist[next.arr] = next.dist + cur.dist;
+			pq.emplace(cur.dist + next.dist, next.arr);
 		}
 	}
 
 	for (int i = 1; i <= v; i++)
 	{
-		if (d[i] == 1e9)
-			cout << "INF" << '\n';
+		if (total_dist[i] != INF)
+			cout << total_dist[i] << '\n';
 		else
-			cout << d[i] << '\n';
+			cout << "INF" << '\n';
 	}
 }
+
+
+// 2026-03-05 (¸ñ)
+
+// #include <iostream>
+// #include <vector>
+// #include <queue>
+// 
+// using namespace std;
+// 
+// int main()
+// {
+// 	ios::sync_with_stdio(false);
+// 	cin.tie(nullptr);
+// 
+// 	int v, e, k;
+// 	cin >> v >> e >> k;
+// 
+// 	vector<vector<pair<int, int>>> con_list(v + 1);
+// 	vector<int> d(v + 1, 1e9);
+// 	priority_queue<pair<int, int>, 
+// 		vector<pair<int, int>>, greater<pair<int, int>>> pq;
+// 
+// 	for (int i = 0; i < e; i++)
+// 	{
+// 		int dep, arr, dist;
+// 		cin >> dep >> arr >> dist;
+// 
+// 		con_list[dep].emplace_back(dist, arr);
+// 	}
+// 
+// 	pq.emplace(0, k);
+// 	d[k] = 0;
+// 
+// 	while (!pq.empty())
+// 	{
+// 		auto cur = pq.top(); pq.pop();
+// 		int cd = cur.first;
+// 		int carr = cur.second;
+// 
+// 		if (d[carr] < cd) continue;
+// 
+// 		for (auto next : con_list[carr])
+// 		{
+// 			int nd = next.first;
+// 			int narr = next.second;
+// 
+// 			if (cd + nd >= d[narr]) continue;
+// 
+// 			pq.emplace(cd + nd, narr);
+// 			d[narr] = cd + nd;
+// 		}
+// 	}
+// 
+// 	for (int i = 1; i <= v; i++)
+// 	{
+// 		if (d[i] == 1e9)
+// 			cout << "INF" << '\n';
+// 		else
+// 			cout << d[i] << '\n';
+// 	}
+// }
